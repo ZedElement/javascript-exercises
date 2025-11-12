102ada6dce0df15dd346106b48e8f7ca227973b3
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-13 01:19:46 +0400

    finished assignment 06 repeatString using array method. will try again using strings only with another try.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let array = [];
+    if (num < 0){
+      return 'ERROR';
+    }
+    for (i = 0; i < num; i++){
+      array.push(string);
+    }
+    return array.join('');
 };
+
+repeatString('hey', 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
