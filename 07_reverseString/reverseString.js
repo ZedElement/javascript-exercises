da327b4edce65270b4a5ed517c69fec86ff5dd8c
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-13 23:49:41 +0400

    resolved assignment 07_reverseString.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splittedArr = string.split("");
+  let reversedArr = splittedArr.reverse();
+  let joinedArr = reversedArr.join("");
+  return joinedArr;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
