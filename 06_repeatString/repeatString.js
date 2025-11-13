2f616e2713e1a6ae61b8847fba8119666e0f6bce
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-13 22:54:42 +0400

    Added another solution to 06_repeatString that only utilizes strings.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -12,5 +12,21 @@ const repeatString = function(string, num) {
 repeatString('hey', 3);
 
 
+// Another soution that relies only on strings
+
+// const repeatString = function(string, num){
+//   let word = "";
+//   if (num < 0){
+//     return 'ERROR';
+//   }
+//   for (i = 0; i < num; i++){
+//     word += string;
+//   }
+//   return word;
+// }
+
+// console.log(repeatString('hey', 3));
+
+
 // Do not edit below this line
 module.exports = repeatString;
