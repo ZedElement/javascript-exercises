ccc9e7978024ee40441f39936cdf0a1d77a33ed4
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-18 07:13:56 +0400

    resolved assignment 08 using for loop and splice()

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToBeRemoved) {
+    const removeArray = Array.from(itemsToBeRemoved);
+
+    for (let i = array.length - 1; i >= 0; i--){
+        if (removeArray.includes(array[i])){
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
