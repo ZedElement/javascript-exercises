9d4b9c3adf54747515a872e90c3811f9f8022530
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-19 23:21:54 +0400

    resolved assignment 10 leapYears.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (
+        (year % 400 === 0) ||
+        (year % 4 === 0 && year % 100 !== 0)
+    ){
+        return true
+        // return `${year} is a leap year.`
+    } else {
+        return false
+        // return `${year} is not a leap year.`
+    }
 };
 
 // Do not edit below this line
