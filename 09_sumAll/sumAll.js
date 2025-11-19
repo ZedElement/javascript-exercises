364bba243c60defbd148cf67a30d9d2f9edab128
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-19 21:31:11 +0400

    resolved the sumAll assignment but I think that my code can be minmised to in less code but with same functionality.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let sumTotal = 0;
+  if (
+    a < 1 || b < 1
+    || !Number.isInteger(a)
+    || !Number.isInteger(b)
+  ){
+    return 'ERROR'
+  }
+  for (let i = a; i <= b; i++){
+    if (a < b){
+      sumTotal += i;
+    }
+  }
+  for (let i = b; i <= a; i++){
+    if (a > b){
+      sumTotal += i;
+    }
+  }
+  return sumTotal;
 };
 
 // Do not edit below this line
