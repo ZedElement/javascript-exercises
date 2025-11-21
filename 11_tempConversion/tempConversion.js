37f41a40a178ace09b68ce6ccbb67b5b61dc9dd4
Mohamed Kamal <8843604+ZedElement@users.noreply.github.com>
2025-11-21 16:52:01 +0400

    resolved the tempConversion assignment as required.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(firstTemperature){
+  let convertedTemp = (firstTemperature - 32) * (5/9);
+  let roundedConvertedTemp = Math.round(convertedTemp * 10) / 10;
+  return roundedConvertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(secondTemperature) {
+  let convertedTemperature = (secondTemperature * (9/5)) + 32;
+  let roundedConvertedTemperature = Math.round(convertedTemperature * 10) / 10;
+  return roundedConvertedTemperature;
 };
 
 // Do not edit below this line
